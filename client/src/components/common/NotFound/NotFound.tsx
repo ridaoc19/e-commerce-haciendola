c90a3a9e1673cff7ea4a8b0a08b8936a2d43f598
ricardo <ridaoc19@gmail.com>
2024-05-13 11:51:13 +0200

    fix: style not found

--- client/src/components/common/NotFound/NotFound.tsx
+++ client/src/components/common/NotFound/NotFound.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Svg from '../../assets/icons/Svg';
+import Button from '../button/Button';
+import { useNavigate } from "react-router-dom";
 
 const NotFound: React.FC<{ text?: string }> = ({ text }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="not-found-container">
+      {Svg({type: 'logo'})}
       <h1>404 - Página no encontrada</h1>
       <p>Lo sentimos, la página {!!text && `"${text}"`} que buscas no está disponible.</p>
+      <Button
+      button={{
+        type: 'dark',
+        text: 'Página Principal',
+        handleClick: ()=>{
+          navigate("/");
+        } 
+      }}
+      />
     </div>
   );
 };
